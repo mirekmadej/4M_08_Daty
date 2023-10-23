df1446c222f683397e48a965205b742957adc7d3
Mirek Madej <m.madej@zsp3zamosc.pl>
2023-10-23 15:01:17 +0200

    Dodaj pliki projektów.

---
+++ 4M_08_Daty/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace _4M_08_Daty
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnObliczClicked(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ 4M_08_Daty/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace _4M_08_Daty
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
