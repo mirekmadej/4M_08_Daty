9aa7841740019698116d0c3e8629246ee13ec39a
Mirek Madej <m.madej@zsp3zamosc.pl>
2023-10-23 15:41:06 +0200

    dodano obsługę dat

--- 4M_08_Daty/MainPage.xaml.cs
+++ 4M_08_Daty/MainPage.xaml.cs
@@ -1,15 +1,26 @@
-﻿namespace _4M_08_Daty
+﻿
+
+namespace _4M_08_Daty
 {
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
+            var dzis = DateTime.Today;
+            dateStart.MinimumDate = dzis;
+            dateKoniec.MinimumDate = dzis.AddDays(1);
         }
-
+        private void datePoczatekSelected(object sender, EventArgs e)
+        {
+            var dStart = dateStart.Date;
+            dStart  = dStart.AddDays(1);
+            dateKoniec.MinimumDate = dStart;
+        }
         private void btnObliczClicked(object sender, EventArgs e)
         {
-            
+            var ile = (dateKoniec.Date - dateStart.Date);
+            lblLiczbaDni.Text = ile.Days.ToString();
         }
     }
 }
